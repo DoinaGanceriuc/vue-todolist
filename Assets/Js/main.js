4099c28d8dd6fac56c6e42aa12dc4f49619ed3c7
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-02 12:00:45 +0100

    add v-for loop

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -4,9 +4,20 @@
 - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
  */
 
+/* Strumenti:
+-array
+-v-for
+
+*/
+
 
 const app = new Vue ({
   el: "#root",
   data: {
+    items: [
+      "Fare i compiti",
+      "Fare la spesa",
+      "Fare il bucato"
+    ]
   }
 })
