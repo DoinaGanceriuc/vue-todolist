0234172a63e0b0503b5ddb75213b1e9595c11352
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-02 15:57:53 +0100

    add completed and removed methods

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -28,21 +28,36 @@ const app = new Vue ({
       "Fare i compiti",
       "Fare la spesa",
       "Fare il bucato"
-    ]
+    ],
+    error: false,
+    completed: false
   },
   methods: {
     removeItem(i) {
       console.log("cliccato sulla x");
       this.items.splice(i, 1)
+      this.completed = false
     },
     completedItems(i) {
       console.log("cliccato sul check");
-      this.items.splice(i, 1)
-
+      this.items.splice(i, 1);
+      this.completed = true
+      
+      if(this.items.length == 0) {
+        this.completed = false
+      }
     },
     addItem() {
       console.log("cliccato su aggiungi");
-      this.items.push(this.newItem)
+      if(this.newItem.length > 5) {
+                this.items.push(this.newItem)
+                this.error = false;
+            } else {
+                this.error = true;
+            }
+            
+            this.newItem = ""
+            this.completed = false
     }
 
 
